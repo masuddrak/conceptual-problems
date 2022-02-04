15be0728574324695fcdec798e43f1a4a2d29a71
masudrana <masudranaacb@gmail.com>
2022-02-04 17:49:49 +0600

    object in array acces

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
-// write a function pizza delivery.one is $10 price. you can only order pizza another product is not allow.you can order 1-20 pizza.20pizza over obder than show is out of stock on order 1-20 pizza.
-function foodPunda(foodName, quntity){
-    if(foodName.toLowerCase() !='pizza'){
-        return 'just pizza delavary other product is no delivary';
-    }
-    if(quntity >10){
-        return 'pizza 20 more than is not stock';
-    }
-    const price=quntity*10;
-    return price;
+// object creation
+let student={
+    studentRoll:[1,2,5,2,5,4],
+    name:'Lizza',
+    age:27,
+    height:'5 feet',
+    color:'gray'
 }
-console.log(foodPunda('pizza',10));
+
+console.log(student.studentRoll[2]);
