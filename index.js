c246c413c88143a79bb3bd2ddc0a3f71c17562a3
masudrana <masudranaacb@gmail.com>
2022-02-04 18:35:34 +0600

    switch case

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-// object creation
-let student={
-    studentRoll:[1,2,5,2,5,4],
-    name:'Lizza',
-    age:27,
-    height:'5 feet',
-    color:'gray'
+// switch case 
+const age=40;
+switch(age){
+    case 40:
+        console.log("od person");
+        break;
+    case 25:
+        console.log("young");
+        break;
+    default:
+        console.log('child');    
 }
-
-console.log(student.studentRoll[2]);
