527b924f5fc1de18b0f1bd5b40f02861863867cf
masudrana <masudranaacb@gmail.com>
2022-02-04 17:34:05 +0600

    pizza delivary solution

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-const names='ryhan';
-const age=27;
-const message=`The name is = ${names} & your age=${age}`;
-console.log(message);
+// write a function pizza delivery.one is $10 price. you can only order pizza another product is not allow.you can order 1-20 pizza.20pizza over obder than show is out of stock on order 1-20 pizza.
+function foodPunda(foodName, quntity){
+    if(foodName.toLowerCase() !='pizza'){
+        return 'just pizza delavary other product is no delivary';
+    }
+    if(quntity >10){
+        return 'pizza 20 more than is not stock';
+    }
+    const price=quntity*10;
+    return price;
+}
+console.log(foodPunda('pizza',10));
